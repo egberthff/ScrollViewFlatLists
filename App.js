7bec10a72b0cd2301545386faaf8faae1bb0d61d
egberthff <sherwingoerge@gmail.com>
2023-11-10 20:58:00 +0800

    a

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  ScrollView  from "./src/Scroll";
+import  ScrollView  from "./src/Flatlists";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView/>
       <StatusBar style="auto" />
     </View>
   );
